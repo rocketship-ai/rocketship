762cd04c68a89c2f59ad1fcdf1bcb8e9e163ab62
Matteo Agius <magiusdarrigo@gmail.com>
2025-05-11 10:40:16 -0400

    move types to separate file

--- internal/orchestrator/engine.go
+++ internal/orchestrator/engine.go
@@ -6,38 +6,12 @@ import (
 	"encoding/hex"
 	"fmt"
 	"log"
-	"sync"
 	"time"
 
 	"github.com/rocketship-ai/rocketship/internal/api/generated"
 	"github.com/rocketship-ai/rocketship/internal/dsl"
 	"go.temporal.io/sdk/client"
 )
-
-type Engine struct {
-	generated.UnimplementedEngineServer
-	temporal client.Client
-	runs     map[string]*RunInfo
-	mu       sync.RWMutex
-}
-type RunInfo struct {
-	ID        string
-	Name      string
-	Status    string
-	StartedAt time.Time
-	EndedAt   time.Time
-	Tests     map[string]*TestInfo // Test's WorkflowID : TestInfo
-	Logs      []string
-}
-
-type TestInfo struct {
-	WorkflowID string
-	Name       string
-	Status     string
-	StartedAt  time.Time
-	EndedAt    time.Time
-	RunID      string
-}
 
 func NewEngine(c client.Client) *Engine {
 	return &Engine{
---
+++ internal/orchestrator/types.go
@@ -0,0 +1,34 @@
+package orchestrator
+
+import (
+	"sync"
+	"time"
+
+	"github.com/rocketship-ai/rocketship/internal/api/generated"
+	"go.temporal.io/sdk/client"
+)
+
+type Engine struct {
+	generated.UnimplementedEngineServer
+	temporal client.Client
+	runs     map[string]*RunInfo
+	mu       sync.RWMutex
+}
+type RunInfo struct {
+	ID        string
+	Name      string
+	Status    string
+	StartedAt time.Time
+	EndedAt   time.Time
+	Tests     map[string]*TestInfo // Test's WorkflowID : TestInfo
+	Logs      []string
+}
+
+type TestInfo struct {
+	WorkflowID string
+	Name       string
+	Status     string
+	StartedAt  time.Time
+	EndedAt    time.Time
+	RunID      string
+}
