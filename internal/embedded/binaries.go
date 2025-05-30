f1e9da4c2fa26d9d9ce1fa67b57891cd2abb0e84
Matteo Agius <magiusdarrigo@gmail.com>
2025-05-30 11:47:42 -0400

    v1.5.2

--- internal/embedded/binaries.go
+++ internal/embedded/binaries.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	githubReleaseURL = "https://github.com/rocketship-ai/rocketship/releases/download/%s/%s"
-	DefaultVersion   = "v1.5.1" // This should be updated with each release
+	DefaultVersion   = "v1.5.2" // This should be updated with each release
 )
 
 type binaryMetadata struct {
