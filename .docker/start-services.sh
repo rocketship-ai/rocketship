e7ed41ef551f541566471996818e5c61f2be947e
Matteo Agius <magiusdarrigo@gmail.com>
2025-07-06 16:06:07 -0700

    chore: remove .docker files that don't exist in main and update .gitignore

--- .docker/start-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Start services for this worktree
-cd "$(dirname "$0")"
-echo "Starting services for rocketship-rocketship-workflow-enhancements..."
-docker-compose --env-file .env.local -p rocketship-rocketship-workflow-enhancements up -d
-echo "Services started! Temporal UI: http://localhost:8129"
--- .docker/stop-services.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Stop services for this worktree
-cd "$(dirname "$0")"
-echo "Stopping services for rocketship-rocketship-workflow-enhancements..."
-docker-compose -p rocketship-rocketship-workflow-enhancements down -v
-echo "Services stopped and cleaned up."
