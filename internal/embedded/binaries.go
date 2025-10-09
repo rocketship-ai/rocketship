12ef465de93be6f34442d96727a5785836d9640b
Matteo Agius <magiusdarrigo@gmail.com>
2025-10-08 22:41:03 -0400

    remove dead code and bump to v0.5.28

--- internal/embedded/binaries.go
+++ internal/embedded/binaries.go
@@ -13,7 +13,7 @@ import (
 
 const (
 	githubReleaseURL = "https://github.com/rocketship-ai/rocketship/releases/download/%s/%s"
-	DefaultVersion   = "v0.5.27" // This should be updated with each release
+	DefaultVersion   = "v0.5.28" // This should be updated with each release
 )
 
 type binaryMetadata struct {
